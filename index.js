f359b532d351b2f7bc267e25da348514c80a18ef
Leonard.Xu <leixu@txtek.cn>
2016-05-13 13:30:41 +0800

    tiny fixed

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-var _config = require('./_config.js'),
+var config = require('./_config.js'),
 	http = require('http'),
 	app = require('./src/server.js'),
 	moment = require('moment');
 
-//创建 http 服务，并将 socket.io 监听到该 http 服务
 httpServer = http.createServer(app);
 
-//加载socket.io
+//load socket.io
 require('./src/io.js');
 
 //start the server
-httpServer.listen(_config.http_server_port || 2345, function () {
+httpServer.listen(config.http_server_port || 2345, function () {
 	console.log(moment().format("YYYY-MM-DD HH:mm:ss") + '**************Server Started**************');
 });
